94ce30fdd7d2590cfde4ca4bf5e13aa93064a878
s8frbroy <broyfrederik01@gmail.com>
2021-12-11 14:17:30 +0100

    data preprocessing, remove rows, cols....

--- util.py
+++ util.py
@@ -4,6 +4,11 @@ import numpy as np
 
 
 def load_multi_data(number):
+    '''
+    :param number: number of splitted file to use
+    :return: dataframe
+    '''
+
     temp = "sensor_sample_float_"
     source = "multi/" + temp + "0" + ".csv"
     dic = pd.read_csv(source)
@@ -27,6 +32,11 @@ def prepare_time_multi(multi):
 
 
 def load_aras(house, days):
+    '''
+    :param house: "HouseA" or "HouseB"
+    :param days:  how many days you want to load, 1 day = 86400 lines
+    :return: dataframe
+    '''
     temp = "DAY_"
     names = [str(x) for x in range(0, 22)]
     dic = pd.read_csv("Aras/" + house + "/" + "DAY_1" + ".txt", sep=" ", names=names)
@@ -37,4 +47,54 @@ def load_aras(house, days):
         day_cur = pd.read_csv(source, sep=" ", names=names)
         day_cur['time'] = list(range(1, 86401))
         dic = pd.concat([dic, day_cur])
+    dic = remove_sensors_not_used(dic, house)
+    dic = remove_rows_Aras(dic)
     return dic
+
+
+def remove_sensors_not_used(df, house):
+    '''
+    :param df: dataframe
+    :param house: specify "HouseA" or "HouseB"
+    :return: dataframe only with the usable sensor
+    '''
+
+    if house == "HouseA":
+        return remove_sensors_HouseA(df)
+    else:
+        return remove_sensors_HouseB(df)
+
+
+def remove_sensors_HouseA(df):
+    '''
+    :param df: df
+    :return: df with sensor: 4,5,8,12,13,14,16,17,18,20
+    '''
+    sensors_keep = np.array(["3", "4", "7", "11", "12", "13", "15", "16", "17", "19", "20", "21", "time"])
+    return df.loc[:, sensors_keep]
+
+
+def remove_sensors_HouseB(df):
+    '''
+    :param df: df
+    :return: df with sensor:
+    '''
+    sensors_keep = np.array(["2", "5", "6", "10", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "time"])
+    return df.loc[:, sensors_keep]
+
+def remove_rows_Aras(df):
+    '''
+    :param df: Aras: HouseA or HouseB
+    :return: df without duplicate rows
+    '''
+    cur = df.iloc[0,:]
+    for index,row in df.iterrows():
+        if index == 0:
+            pass
+        else:
+            pre = cur
+            cur = row
+            comp = cur[:-1] == pre[:-1]
+            if comp.all():
+                df = df.drop([index-1])
+    return df
